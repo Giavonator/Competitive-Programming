d7607afccfc96431fbbd0421ec4a2e59601d99d7
Giavonator <68939873+Giavonator@users.noreply.github.com>
2024-03-08 21:01:29 -0500

    Add files via upload

--- USACO/Practice/forestQueries.cpp
+++ USACO/Practice/forestQueries.cpp
@@ -6,32 +6,38 @@ int main(){
 	int n, q;
 	cin >> n >> q;
 
-	vector<vector<int> > grid(n, vector<int>(n));
+	vector<vector<int> > grid(n + 1, vector<int>(n + 1));
 	
-	for(int i = 0; i < n; i++){
+	for(int i = 1; i <= n; i++){
 		string line; cin >> line;
 
-		for(int x = 0; x < n; x++){
-			int num = line[x] == '*' ? 1 : 0;
-
-			if(x == 0) grid[i][0] = num;
-			else grid[i][x] = grid[i][x-1] + num;
+		for(int x = 1; x <= n; x++){
+			int num = line[x-1] == '*' ? 1 : 0;
+			grid[i][x] = grid[i][x-1] + num;
 
 		}
 	}
 
+	for(int i = 1; i <= n; i++){
 
-	
-	for(int i = 0; i < n; i++){
+		for(int x = 1; x <= n; x++){
 
-		for(int x = 0; x < n; x++){
-			cout << grid[i][x] << " ";
+			grid[x][i] += grid[x-1][i];
+
 		}
-		cout << "\n";
 	}
 
+	for(int i = 0; i < q; i++){
+		int y1, x1, y2, x2;
+		cin >> y1 >> x1 >> y2 >> x2;
 
+		int num = grid[y2][x2];
+		int w = x2 - x1 + 1;
+		int h = y2 - y1 + 1;
+		num -= grid[y2][x2 - w] + grid[y2 - h][x2] - grid[y2 - h][x2 - w];
+		printf("%d\n", num);
 
+	}
 
 
 
